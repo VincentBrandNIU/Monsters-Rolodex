5c4f3187f2f5813fbbb71257f45af5df084bdacf
Vincent Brand <vincentbrandniu@gmail.com>
2020-03-22 16:45:53 -0500

    Updates

---
+++ precache-manifest.0d8445ac41ec8609f08cab1e62ffb941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5487240c261a562d281b6baa5e9dbef9",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "13cfbd5f1e301feeb045",
+    "url": "/Monsters-Rolodex/static/css/main.277298b2.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/Monsters-Rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monsters-Rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cfbd5f1e301feeb045",
+    "url": "/Monsters-Rolodex/static/js/main.3015436e.chunk.js"
+  },
+  {
+    "revision": "7f9d23ce389d419af742",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.74e1ea2f.js"
+  }
+]);
